931ff09866f4c4d1213ede891ae89f0912af0409
Kyle Manna <kyle@kylemanna.com>
2016-05-04 22:45:54 -0700

    k22f: Hack in values for 16MHz crystal
    * Attempt to hack in the external crystal

--- cmsis-core-k64f/system_MK64F12.h
+++ cmsis-core-k64f/system_MK64F12.h
@@ -155,7 +155,8 @@ extern "C" {
 
 /* RTC oscillator setting */
 /* RTC_CR: SC2P=0,SC4P=0,SC8P=0,SC16P=0,CLKO=1,OSCE=1,WPS=0,UM=0,SUP=0,WPE=0,SWR=0 */
-#define SYSTEM_RTC_CR_VALUE            0x0300U             /* RTC_CR */
+// Disable RTC on K22F, VBAT is unpowered and generates a hardfault
+//#define SYSTEM_RTC_CR_VALUE            0x0300U             /* RTC_CR */
 
 /* Low power mode enable */
 /* SMC_PMPROT: AVLP=1,ALLS=1,AVLLS=1 */
@@ -191,7 +192,8 @@ extern "C" {
 /* SIM_CLKDIV1: OUTDIV1=0,OUTDIV2=0,OUTDIV3=1,OUTDIV4=1 */
   #define SYSTEM_SIM_CLKDIV1_VALUE     0x00110000U         /* SIM_CLKDIV1 */
 /* SIM_SOPT1: USBREGEN=0,USBSSTBY=0,USBVSTBY=0,OSC32KSEL=2,RAMSIZE=0 */
-  #define SYSTEM_SIM_SOPT1_VALUE       0x00080000U         /* SIM_SOPT1 */
+  //#define SYSTEM_SIM_SOPT1_VALUE       0x00080000U         /* SIM_SOPT1 */
+  #define SYSTEM_SIM_SOPT1_VALUE       0x00U         /* SIM_SOPT1 */
 /* SIM_SOPT2: SDHCSRC=0,TIMESRC=0,RMIISRC=0,USBSRC=0,PLLFLLSEL=0,TRACECLKSEL=0,PTD7PAD=0,FBSL=0,CLKOUTSEL=0,RTCCLKOUTSEL=0 */
   #define SYSTEM_SIM_SOPT2_VALUE       0x00U               /* SIM_SOPT2 */
 #elif (CLOCK_SETUP == 1)
@@ -279,17 +281,22 @@ extern "C" {
   #define DEFAULT_SYSTEM_CLOCK         120000000u          /* Default System clock value */
   #define MCG_MODE                     MCG_MODE_PEE /* Clock generator mode */
   /* MCG_C1: CLKS=0,FRDIV=7,IREFS=0,IRCLKEN=1,IREFSTEN=0 */
-  #define SYSTEM_MCG_C1_VALUE          0x3AU               /* MCG_C1 */
+  //#define SYSTEM_MCG_C1_VALUE          0x3AU               /* MCG_C1 */
+  #define SYSTEM_MCG_C1_VALUE          0x2U               /* MCG_C1 */
   /* MCG_C2: LOCRE0=0,FCFTRIM=0,RANGE=2,HGO=0,EREFS=0,LP=0,IRCS=0 */
-  #define SYSTEM_MCG_C2_VALUE          0x20U               /* MCG_C2 */
+  //#define SYSTEM_MCG_C2_VALUE          0x20U               /* MCG_C2 */
+  #define SYSTEM_MCG_C2_VALUE          0xa4U               /* MCG_C2 */
   /* MCG_C4: DMX32=0,DRST_DRS=0,FCTRIM=0,SCFTRIM=0 */
-  #define SYSTEM_MCG_C4_VALUE          0x00U               /* MCG_C4 */
+  //#define SYSTEM_MCG_C4_VALUE          0x00U               /* MCG_C4 */
+  #define SYSTEM_MCG_C4_VALUE          0x14U               /* MCG_C4 */
   /* MCG_SC: ATME=0,ATMS=0,ATMF=0,FLTPRSRV=0,FCRDIV=0,LOCS0=0 */
   #define SYSTEM_MCG_SC_VALUE          0x00U               /* MCG_SC */
 /* MCG_C5: PLLCLKEN0=0,PLLSTEN0=0,PRDIV0=0x13 */
-  #define SYSTEM_MCG_C5_VALUE          0x13U               /* MCG_C5 */
+  //#define SYSTEM_MCG_C5_VALUE          0x13U               /* MCG_C5 */
+  #define SYSTEM_MCG_C5_VALUE          0x43U               /* MCG_C5 */
 /* MCG_C6: LOLIE0=0,PLLS=1,CME0=0,VDIV0=0x18 */
-  #define SYSTEM_MCG_C6_VALUE          0x58U               /* MCG_C6 */
+  //#define SYSTEM_MCG_C6_VALUE          0x58U               /* MCG_C6 */
+  #define SYSTEM_MCG_C6_VALUE          0x46U               /* MCG_C6 */
 /* MCG_C7: OSCSEL=0 */
   #define SYSTEM_MCG_C7_VALUE          0x00U               /* MCG_C7 */
 /* OSC_CR: ERCLKEN=1,EREFSTEN=0,SC2P=0,SC4P=0,SC8P=0,SC16P=0 */
@@ -297,9 +304,11 @@ extern "C" {
 /* SMC_PMCTRL: LPWUI=0,RUNM=0,STOPA=0,STOPM=0 */
   #define SYSTEM_SMC_PMCTRL_VALUE      0x00U               /* SMC_PMCTRL */
 /* SIM_CLKDIV1: OUTDIV1=0,OUTDIV2=1,OUTDIV3=1,OUTDIV4=4 */
-  #define SYSTEM_SIM_CLKDIV1_VALUE     0x01140000U         /* SIM_CLKDIV1 */
+  //#define SYSTEM_SIM_CLKDIV1_VALUE     0x01140000U         /* SIM_CLKDIV1 */
+  #define SYSTEM_SIM_CLKDIV1_VALUE     0x01440000U         /* SIM_CLKDIV1 */
 /* SIM_CLKDIV2: USBDIV=4,USBFRAC=1 */
-  #define SYSTEM_SIM_CLKDIV2_VALUE     0x09U               /* SIM_CLKDIV2 */
+  //#define SYSTEM_SIM_CLKDIV2_VALUE     0x09U               /* SIM_CLKDIV2 */
+  #define SYSTEM_SIM_CLKDIV2_VALUE     0x00U               /* SIM_CLKDIV2 */
 /* SIM_SOPT1: USBREGEN=0,USBSSTBY=0,USBVSTBY=0,OSC32KSEL=2,RAMSIZE=0 */
   #define SYSTEM_SIM_SOPT1_VALUE       0x00080000U         /* SIM_SOPT1 */
 /* SIM_SOPT2: SDHCSRC=0,TIMESRC=0,RMIISRC=0,USBSRC=0,PLLFLLSEL=1,TRACECLKSEL=0,PTD7PAD=0,FBSL=0,CLKOUTSEL=0,RTCCLKOUTSEL=0 */
